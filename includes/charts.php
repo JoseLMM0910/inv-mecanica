5aba3c2542f789208b6f390672f33cf65d0768fd
Jose Moreno <josephmarklinus@gmail.com>
2021-06-28 21:40:29 -0400

    tercer commit

--- includes/charts.php
+++ includes/charts.php
@@ -6,20 +6,21 @@
       google.charts.setOnLoadCallback(drawChart);
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawLinearChart);
+      google.charts.setOnLoadCallback(drawDonutChart);
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
+          ['Mes', 'Ventas', 'Gastos', 'Ganancias'],
+          ['Enero', 1000, 400, 200],
+          ['Febrero', 1170, 460, 250],
+          ['Marzo', 660, 1120, 300],
+          ['Abril', 1030, 540, 350]
         ]);
 
         var options = {
           chart: {
             title: 'Desarrollo de la empresa',
-            subtitle: 'Ventas, Gastos y Ganancias: 2014-2017',
+            subtitle: 'Ventas, Gastos y Ganancias: Enero-Abril',
           }
         };
 
@@ -31,21 +32,38 @@
       function drawLinearChart(){
         
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales'],
-          ['2004',  1000],
-          ['2005',  1170],
-          ['2006',  660],
-          ['2007',  1030]
+          ['Mes', 'ganancias'],
+          ['Enero',  1000],
+          ['Febrero',  2000],
+          ['Marzo',  4000],
+          ['Abril',  5500]
         ]);
 
         var options = {
-          title: 'Company Performance',
+          title: 'Ganancias de la empresa',
           curveType: 'function',
           legend: { position: 'bottom' }
         };
 
         var chart = new google.visualization.LineChart(document.getElementById("chart_div2"));
 
+        chart.draw(data, options);
+      }
+      function drawDonutChart(){
+        var data = google.visualization.arrayToDataTable([
+          ['Productos', 'Cantidad'],
+          ['Materiales',     11],
+          ['Maquinaria',      2],
+          ['Consumibles',  2],
+          ['Transito', 2],
+        ]);
+
+        var options = {
+          title: 'Productos en almacen',
+          is3D: false,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div3'));
         chart.draw(data, options);
       }
 
